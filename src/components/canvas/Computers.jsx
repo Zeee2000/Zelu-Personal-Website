a02fe2691cf2445915d91ef9363acd342ff8e409
Zeee2000 <zelutesema@gmail.com>
2025-02-24 17:55:54 +0200

    Changing camera  for computer model on mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,7 +22,7 @@ const Computers = ({ isMobile }) => {
         <primitive
             object={computer.scene}
             scale={isMobile ? 25 : 30}
-            position={isMobile ? [0, -3, -2.2] : [2, -2, -1.5]}
+            position={isMobile ? [2, -2, -1.5] : [2, -2, -1.5]}
             rotation={[0, 2, 0]}
         />
       </mesh>
