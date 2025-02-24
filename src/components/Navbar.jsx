4c50dd7dfbd1625fa51ebfa514b8b7ea3f26e769
Zeee2000 <zelutesema@gmail.com>
2025-02-24 17:34:15 +0200

    Editing of logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+import zeluLogo from "../assets/zelu-logo.png";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -42,7 +43,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src="./assets/zelu-logo.png" alt="./assets/zelu-logo.png" className='w-9 h-9 object-contain' />
+          <img src={zeluLogo} alt="./assets/zelu-logo.png" className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Zelu &nbsp;
             <span className='sm:block hidden'> | Cloud Engineer </span>
