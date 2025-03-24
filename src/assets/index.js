7a5a27be66ef51753ac981234eaec3971df6737b
Zeee2000 <zelutesema@gmail.com>
2025-03-24 17:00:02 +0200

    Removed unused assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,20 +6,7 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-
 import pythonLogo from "./python-logo.svg";
-import javaLogo from "./java-logo.svg";
-import javascriptLogo from "./javascript-logo.svg";
-import saplogo from "./sap-logo.svg";
 import frontendLogo from "./front-end-logo.png";
 
 import entelectLogo from "./company/EntelectLogo.png";
@@ -34,19 +21,7 @@ export {
   github,
   menu,
   close,
-  css,
-  docker,
-  git,
-  html,
-  javascript,
-  nodejs,
-  reactjs,
-  tailwind,
-  typescript,
   pythonLogo,
-  javaLogo,
-  javascriptLogo,
-  saplogo,
   frontendLogo,
   entelectLogo,
   epiuseLogo,
